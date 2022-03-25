9d6d67d520f1a6b7905231ea98b08562a3d92d31
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-03-25 18:43:29 +0000

    added GuessingNumber

--- GuessingNumber.cpp
+++ GuessingNumber.cpp
@@ -4,27 +4,35 @@
 // Created on: March 2022
 // This program lets the user try and guess the correct number between 0 and 9
 
+
 #include <iostream>
+#include <random>
+
 
 int main() {
-    // this function lets the user
-    // try and guess the correct number between 0 and 9
-    const int SET_NUMBER = 4;
+    // this function lets the user guess a random number between 0 and 9
+    int someRandomNumber;
     int guessedNumber;
+
+    std::random_device rseed;
+    std::mt19937 rgen(rseed());  // mersenne_twister
+    std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
+    someRandomNumber = idist(rgen);
 
     // input
     std::cout << "Enter a number between 0 and 9: ";
     std::cin >> guessedNumber;
-    std::cout << "" << std::endl;
 
     // process
-    if (guessedNumber == SET_NUMBER) {
+    if (guessedNumber == someRandomNumber) {
         // output
         std::cout << "You guessed correct!";
-        std::cout << "\nDone." << std::endl;
-    } else if (guessedNumber != SET_NUMBER) {
+    } else if (guessedNumber != someRandomNumber) {
         // output
         std::cout << "You guessed wrong!";
-        std::cout << "\nDone." << std::endl;
+        std::cout << "\nThe correct answer was "
+        << someRandomNumber << std::endl;
     }
+
+    std::cout << "\nDone." << std::endl;
 }
